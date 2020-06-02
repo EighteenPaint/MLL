d83fba380a2ef869412e7b39ea9357e0f9e5e5c2
Benny.Chan <chenbinkria@163.com>
2020-06-03 00:12:38 +0800

    开始逻辑回归的学习

---
+++ chapter-05/logistic.py
@@ -0,0 +1,59 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+#@author:Benny.Chen
+#@file: logistic.py
+#@time: 2020/6/2 15:48
+"""
+import numpy as np
+
+
+def loadDataSet():
+    """
+    b   x0   x1
+    -----------
+    1   2    3
+    1   2    3
+    1   2    3
+    1   2    3
+    1   2    3
+    1   2    3
+    1   2    3
+    :return:
+    """
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])  # 1.0相当于常数b
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+def sigmoid(inX):
+    return 1.0 / (1 + np.exp(-inX))
+
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = np.mat(dataMatIn)
+    labelMat = np.mat(classLabels).transpose()  # 矩阵转置
+    m, n = np.shape(dataMatrix)
+    alpha = 0.01  # 这也是一个不好确定的数值，很多时候也许要尝试很多次才可以得出来
+    maxCycle = 500  # 循环次数500
+    """
+    dataMatrix * weight
+    1   2    3
+    1   2    3
+    1   2    3  1
+    1   2    3  1 
+    1   2    3  1
+    1   2    3
+    1   2    3    
+    """
+    weights = np.ones((n, 1))
+    for k in range(maxCycle):
+        h = sigmoid(dataMatrix * weights)  # 矩阵运算
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose()*error  # 这种方法处理妙啊，明天推导一下
+    return weights
---
+++ chapter-05/main.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+#@author:Benny.Chen
+#@file: main.py
+#@time: 2020/6/2 15:46
+"""
+import logistic
+if __name__ == '__main__':
+    dataArr,labelMat = logistic.loadDataSet()
+    weights = logistic.gradAscent(dataArr,labelMat)
+    print(weights)
